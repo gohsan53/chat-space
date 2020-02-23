e259da2f9ef2cf68055bcceb1fde554aa5a30725
gohsan53 <s.kitagoh@gmail.com>
2020-02-23 13:07:44 +0900

    add actions in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      render :index
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text, :image)
   end
 end
