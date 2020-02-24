9a4ece17807d2750180c91969048cb85c565efc1
gohsan53 <s.kitagoh@gmail.com>
2020-02-24 15:22:55 +0900

    implement sending message function.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :text, presence: true, unless: :image?
+  validates :text, presence: true, unless: :image
 
   mount_uploader :image, ImageUploader
 end
