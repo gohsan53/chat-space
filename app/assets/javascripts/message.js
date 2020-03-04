73beea3ebab0839aa39477d9935c59dd2d43b05e
gohsan53 <s.kitagoh@gmail.com>
2020-03-04 23:21:39 +0900

    receive JSON by done method to form HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,40 @@
 $(function() {
+  function buildHTML(message){
+    if (message.image) {
+      var html =
+        `<div class='main-chat__message-list'>
+          <div class='list-items'>
+            <span class='list-items__name'>
+              ${message.user_name}
+            </span>
+            <span class='list-items__date'>
+              ${message.created_at}
+            </span>
+            <div class='list-items_message'>
+              ${message.text}
+              <img src=${message.image}, class='list-items__message__image' >
+            </div>
+          </div>
+        </div>`
+      return html;
+    } else {
+      var htmls =
+        `<div class='main-chat__message-list'>
+          <div class='list-items'>
+            <span class='list-items__name'>
+              ${message.user_name}
+            </span>
+            <span class='list-items__date'>
+              ${message.created_at}
+            </span>
+            <div class='list-items_message'>
+              ${message.text}
+            </div>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e) {
     console.log(this);
     e.preventDefault()
@@ -11,6 +47,9 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
     })
   });
 });
