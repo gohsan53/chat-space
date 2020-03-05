f0579284be02cbe6306c34d2106e72ef191a13ec
gohsan53 <s.kitagoh@gmail.com>
2020-03-05 19:10:41 +0900

    enable asynchronous communication at keyup

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
-$(function() {
-  $('#user-search-field').on('keyup', function() {
+$(function () {
+  $('#user-search-field').on('keyup', function () {
     let input = $('#user-search-field').val();
-    console.log(input)
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
+      .done(function (users) {
+        console.log(users);
+      })
+      .fail(function () {
+        console.log('失敗です')
+      })
   })
 });
