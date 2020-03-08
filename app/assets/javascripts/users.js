a4e4436e43c476fe39847307430443dfd52bede7
gohsan53 <s.kitagoh@gmail.com>
2020-03-08 10:55:08 +0900

    displayed the results of incremental search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,23 @@
 $(function () {
+  function addUser(user) {
+    let html = `
+      <div class='chat-group-user clearfix'>
+        <p class='chat-group-user__name'>${user.name}</p>
+        <div class='user-search-add chat-group__btn chat-group-user__btn--add' data-user-id='${user.id}' data-user-name}'>追加</div>
+      </div>
+    `;
+    $('#user-search-result').append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class='chat-group-user clearfix'>
+        <p class='chat-group-user__name'>ユーザーが見つかりません</p>
+      </div>
+    `;
+    $('#user-search-result').append(html);
+  }
+
   $('#user-search-field').on('keyup', function () {
     let input = $('#user-search-field').val();
     $.ajax({
@@ -8,10 +27,20 @@ $(function () {
       data: { keyword: input }
     })
       .done(function (users) {
-        console.log(users);
+        $('#user-search-result').empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
       })
       .fail(function () {
-        console.log('失敗です')
-      })
-  })
+        alert('通信エラーです．ユーザーが表示できません．');
+      });
+  });
 });
