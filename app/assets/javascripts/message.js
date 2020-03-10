473099cb18187f5978b7fb7238f0f62868513bf4
gohsan53 <s.kitagoh@gmail.com>
2020-03-10 10:40:23 +0900

    auto refresh only on main-chat screen and scroll down to the bottom at getting new messages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,15 +66,20 @@ $(function () {
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     });
   };
-  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 2000);
+  }
 });
--- app/views/api/messages/index.json.jbuilder
+++ app/views/api/messages/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.array! @messages do |message|
-  json.text message.content
+  json.text message.text
   json.image message.image.url
   json.created_at message.created_at.strftime('%Y/%m/%d(%a) %H:%M:%S')
   json.user_name message.user.name
